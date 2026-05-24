602ac38dfbcb84f917d9a8d3f581366eaec7599b
Madhusudan <madhusudan04337@gmail.com>
2026-05-24 23:25:59 +0530

    feat: Set up static file serving and add initial HTML and CSS files

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const path = require('path');
 const app = express();
 app.use(express.json());
 app.use(cors());
-app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'public')));
 
 const ai = new GoogleGenAI({
     apiKey: process.env.GEMINI_API_KEY,
